9f9df1782383b10b3737f4b5a820e0c782347f89
Liza Morrison <lizammorrison@gmail.com>
2024-10-29 18:45:05 -0700

    add home page with magicUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import duncan from './duncan.webp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={duncan} className="rounded-full h-48 w-48" alt="logo" />
+        <p className="header">Duncan Morrison</p>
+        <button className="karla p-4 rounded-3xl text-2xl bg-white text-black">
+          Portfolio
+        </button>
       </header>
     </div>
   );
