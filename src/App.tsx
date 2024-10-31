b1ff46eeb05b3989caf540e893cd2aaf517b6973
Liza Morrison <lizammorrison@gmail.com>
2024-10-31 12:14:05 -0700

    add scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import About from "./components/About";
-import Menu from "./components/Menu";
-import ColorTheory from "./portfolio/ColorTheory";
-import DesignConceptsOne from "./portfolio/DesignConceptsOne";
-import DesignConceptsTwo from "./portfolio/DesignConceptsTwo";
-import DrawingForDesigners from "./portfolio/DrawingForDesigners";
-import DrawingOne from "./portfolio/DrawingOne";
-import HistoryOfDesign from "./portfolio/HistoryOfDesign";
-import RenderingOne from "./portfolio/RenderingOne";
+import About from './components/About';
+import Menu from './components/Menu';
+import ScrollToTopButton from './components/ScrollToTop';
+import ColorTheory from './portfolio/ColorTheory';
+import DesignConceptsOne from './portfolio/DesignConceptsOne';
+import DesignConceptsTwo from './portfolio/DesignConceptsTwo';
+import DrawingForDesigners from './portfolio/DrawingForDesigners';
+import DrawingOne from './portfolio/DrawingOne';
+import HistoryOfDesign from './portfolio/HistoryOfDesign';
+import RenderingOne from './portfolio/RenderingOne';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
       <DrawingForDesigners />
       <RenderingOne />
       <HistoryOfDesign />
+      <ScrollToTopButton />
       <div className="paperOverlay"></div>
     </>
   );
---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,43 @@
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+
+import { AiOutlineArrowUp } from 'react-icons/ai';
+
+export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <div className="fixed bottom-8 right-8 z-50">
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="p-3 bg-[#ffff] text-black rounded-full shadow-md hover:bg-customLightPink focus:outline-none transition duration-300 ease-in-out"
+        >
+          <AiOutlineArrowUp size={24} />
+        </button>
+      )}
+    </div>
+  );
+}
