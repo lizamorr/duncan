f72906b613b0701c8de27f9e85a6b1bb2991dc18
Liza Morrison <lizammorrison@gmail.com>
2024-11-02 17:37:49 -0700

    change name color

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -29,7 +29,7 @@ export default function About() {
             className="rounded-full h-40 w-40 lg:h-52 lg:w-52 border border-customLightPink"
             alt="logo"
           />
-          <p className="header pt-2 lg:pt-8 text-customLightPink leading-[50px]">
+          <p className="header pt-2 lg:pt-8 text-customPink leading-[50px]">
             Duncan Morrison
           </p>
           <p className="bebas-neue-regular text-4xl lg:text-5xl pt-6 text-customRed">
