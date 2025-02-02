cf1507081b57eed9b0cafb02e2c2e7133f86ac0f
Liza Morrison <lizammorrison@gmail.com>
2025-02-02 10:00:35 -0800

    remove fashion illustrator

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import About from './components/About';
-import Menu from './components/Menu';
-import ScrollToTopButton from './components/ScrollToTop';
-import ColorTheory from './portfolio/ColorTheory';
-import DesignConceptsOne from './portfolio/DesignConceptsOne';
-import DesignConceptsTwo from './portfolio/DesignConceptsTwo';
-import DrawingForDesigners from './portfolio/DrawingForDesigners';
-import DrawingOne from './portfolio/DrawingOne';
-import FashionIllustrator from './portfolio/FashionIllustrator';
-import HistoryOfDesign from './portfolio/HistoryOfDesign';
-import RenderingOne from './portfolio/RenderingOne';
-import TwoDForDesigners from './portfolio/TwoDForDesigners';
+import About from "./components/About";
+import Menu from "./components/Menu";
+import ScrollToTopButton from "./components/ScrollToTop";
+import ColorTheory from "./portfolio/ColorTheory";
+import DesignConceptsOne from "./portfolio/DesignConceptsOne";
+import DesignConceptsTwo from "./portfolio/DesignConceptsTwo";
+import DrawingForDesigners from "./portfolio/DrawingForDesigners";
+import DrawingOne from "./portfolio/DrawingOne";
+import HistoryOfDesign from "./portfolio/HistoryOfDesign";
+import RenderingOne from "./portfolio/RenderingOne";
+import TwoDForDesigners from "./portfolio/TwoDForDesigners";
 
 function App() {
   return (
@@ -28,7 +27,6 @@ function App() {
       <RenderingOne />
       <TwoDForDesigners />
       <HistoryOfDesign />
-      <FashionIllustrator />
       <ScrollToTopButton />
       <div className="paperOverlay"></div>
     </>
