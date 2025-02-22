8815de222b1a70563ba817cb3f83f9b58a58ec8c
Liza Morrison <lizammorrison@gmail.com>
2025-02-21 21:31:06 -0800

    update the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,46 +15,34 @@ export default function Menu() {
         Fashion Design
       </h2>
       <p
+        onClick={() => handleScroll("history-of-design")}
+        className="hover:underline"
+      >
+        History of Design
+      </p>
+      <p
         onClick={() => handleScroll("fashion-construction")}
         className="hover:underline"
       >
         Fashion Construction
       </p>
-      <p onClick={() => handleScroll("color")} className="hover:underline">
-        Color Theory
+      <p
+        onClick={() => handleScroll("rendering-1")}
+        className="hover:underline"
+      >
+        Rendering
       </p>
       <p
         onClick={() => handleScroll("design-concepts-1")}
         className="hover:underline"
       >
-        Design Concepts I
+        Design Concepts I-II
       </p>
-      <p
-        onClick={() => handleScroll("design-concepts-2")}
-        className="hover:underline"
-      >
-        Design Concepts II
+      <p onClick={() => handleScroll("color")} className="hover:underline">
+        Color Theory
       </p>
       <p onClick={() => handleScroll("drawing-1")} className="hover:underline">
-        Drawing I
-      </p>
-      <p
-        onClick={() => handleScroll("drawing-for-designers")}
-        className="hover:underline"
-      >
-        Drawing for Designers
-      </p>
-      <p
-        onClick={() => handleScroll("rendering-1")}
-        className="hover:underline"
-      >
-        Rendering I
-      </p>
-      <p
-        onClick={() => handleScroll("history-of-design")}
-        className="hover:underline"
-      >
-        History of Design
+        Drawing
       </p>
     </div>
   );
