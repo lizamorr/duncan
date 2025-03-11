04dadbc04e63a7e60b9a98e1a8b21409dac5372c
Liza Morrison <lizammorrison@gmail.com>
2025-03-11 08:46:32 -0700

    add title click scroll to top

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,13 @@
 export const Title = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <h1 className="paperOverlay2 fixed top-0 left-0 w-full bg-white py-4 text-2xl lg:text-4xl z-50 cursive text-center transition-all duration-300 text-[#25555e]">
+    <h1
+      className="cursor paperOverlay2 fixed top-0 left-0 w-full bg-white py-4 text-2xl lg:text-4xl z-50 cursive text-center transition-all duration-300 text-[#25555e] cursor-pointer"
+      onClick={scrollToTop}
+    >
       Duncan Morrison
     </h1>
   );
